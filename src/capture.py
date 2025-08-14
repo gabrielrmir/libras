cb701045ad7e33973deb096458a5a4af0e308098
Gabriel R. <gabrielmio16@gmail.com>
2025-08-14 14:03:41 -0300

    landmarks drawing

--- src/capture.py
+++ src/capture.py
@@ -1,13 +1,14 @@
 import csv
 import cv2
-import landmarker
+from landmarker import Landmarker
+import utils
 
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit(1)
 
-landmarker = landmarker.Landmarker()
+landmarker = Landmarker()
 
 while True:
     ret, frame = cap.read()
@@ -17,9 +18,21 @@ while True:
 
     landmarker.detect_async(frame)
 
+    if hasattr(landmarker.result, "hand_landmarks"):
+        w,h = 640,360
+        hands = landmarker.result.hand_landmarks
+        for hand in hands:
+            utils.draw_hand(frame,hand,(w,h))
+
+        hands = landmarker.result.hand_world_landmarks
+        for hand in hands:
+            utils.draw_hand_lines(frame,hand,(w,h),(w//8,h//8))
+
+
     cv2.imshow('Libras', frame)
     if cv2.waitKey(1) == ord('q'):
         break
 
+landmarker.close()
 cap.release()
 cv2.destroyAllWindows()
---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import cv2
+
+lines = [(0,1),(1,2),(2,3),(3,4),(0,5),(5,6),(6,7),(7,8),(5,9),(9,10),(10,11),(11,12),(9,13),(13,14),(14,15),(15,16),(13,17),(0,17),(17,18),(18,19),(19,20)]
+
+def draw_hand_lines(frame, hand, scale=(1,1), offset=(0,0)):
+    w,h = scale
+    xoff,yoff = offset
+    for line in lines:
+        x1,y1 = (int(hand[line[0]].x*w)+xoff,int(hand[line[0]].y*h)+yoff)
+        x2,y2 = (int(hand[line[1]].x*w)+xoff,int(hand[line[1]].y*h)+yoff)
+        cv2.line(frame,(x1,y1),(x2,y2),(255,0,0),2)
+
+
+def draw_hand(frame, hand, scale=(1,1), offset=(0,0)):
+    w,h = scale
+    xoff,yoff = offset
+
+    draw_hand_lines(frame,hand,scale,offset)
+    for pos in hand:
+        x,y = (int(pos.x*w)+xoff,int(pos.y*h)+yoff)
+        cv2.circle(frame,(x,y), 8, (0,0,255), -1)
+
+
