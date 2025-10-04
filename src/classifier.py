1bfd974b4969fa01b38361f8063246e6f26f606b
Gabriel R. <gabrielmio16@gmail.com>
2025-10-04 09:58:53 -0300

    classificador de gestos

--- src/classifier.py
+++ src/classifier.py
@@ -5,21 +5,55 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 
 from dataset import load_dataset
+from camera import Camera
+import utils
+import cv2
+from landmarker import Landmarker
 
-dataset_path = "./data/dataset.csv"
-dataset = load_dataset(dataset_path)
-X = dataset.data
-y = dataset.labels
+dataset_path = "./data/capture.csv"
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=0)
+# accuracy = accuracy_score(y_test, y_pred)
+# print(accuracy)
 
-clf = Pipeline(
-    steps=[("scaler", StandardScaler()), ("knn", KNeighborsClassifier(n_neighbors=11))]
-)
+class Classifier():
+    def __init__(self, dataset_path):
+        X, y = load_dataset(dataset_path)
+        self.clf = Pipeline(steps=[
+            # ("scaler", StandardScaler()),
+            ("knn", KNeighborsClassifier(n_neighbors=11))
+        ])
+        self.clf.fit(X,y)
+    def predict(self, X):
+        return self.clf.predict(X)
 
-clf.fit(X_train, y_train)
+if __name__ == '__main__':
+    classifier = Classifier(dataset_path)
+    landmarker = Landmarker()
+    cam = Camera()
+    cap = cam.cap
 
-y_pred = clf.predict(X_test)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        key = cv2.waitKey(1)
+        if key == 27: # esc
+            break
 
-accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
+        label = ''
+        landmarker.detect(frame)
+        if landmarker.has_result():
+            hand = landmarker.result.hand_world_landmarks[0]
+            hand = utils.hand_to_1d_array(hand)
+            box = landmarker.get_hands_boundaries()[0]
+            utils.draw_box(frame, box)
+            label = str(classifier.predict([hand]))
+
+
+        cv2.flip(frame, 1, frame)
+        utils.draw_text(frame, label, (10,40))
+        cv2.imshow('Classifier', frame)
+
+    cap.release()
