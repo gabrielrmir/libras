cf0bab21add607cd9244436c2bf42a49fd323bb5
Gabriel R. <gabrielmio16@gmail.com>
2025-11-13 15:35:31 -0300

    comando crop

--- src/main.py
+++ src/main.py
@@ -76,12 +76,42 @@ Opções:
                                     latência; Valor padrão: 0.01""")
 
 def _cmd_stats():
-    import statistics
-    statistics.main()
+    import stats
+    stats.main()
 
+# Obter informações de acurácia de um conjunto de imagens de teste
+# As imagens devem ser dispostas no diretório 'test' onde cada subdiretório
+# corresponde ao label previsto. É importante que o label esteja em letra
+# minúscula. O nome das imagens pode ser completamente arbitrário.
+# Ex.:
+# test/a/test001.jpg
+# test/a/test002.jpg
+# test/a/test003.jpg
+# ...
+# test/b/test001.jpg
+# test/b/test002.jpg
+# test/b/test003.jpg
+# ...
 def _cmd_accuracy():
     import accuracy
     accuracy.main()
+
+# Ferramenta de captura alternativa. Captura região da câmera e salva como
+# imagem. Pode ser informado o label da pose que está sendo capturada
+def _cmd_crop(args):
+    label = 'a'
+    if len(args):
+        label = args.pop(0)
+    import crop
+    crop.main(label)
+
+# Processamento de imagens de treino para dataset em csv
+# Dados processados serão acrescentados no arquivo informado pela opção
+# options.dataset_path
+# As imagens seguem a mesma estrutura informada no comando de acurácia, usando
+# o diretório 'train' no lugar de 'test'.
+def _cmd_process():
+    pass
 
 def main():
     args = sys.argv[1:]
@@ -103,6 +133,10 @@ def main():
             _cmd_stats()
         case 'accuracy':
             _cmd_accuracy()
+        case 'crop':
+            _cmd_crop(args)
+        case 'process':
+            _cmd_process()
         case _:
             _err_invalid()
 
